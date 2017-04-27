e8d0c62c2ca1b92979acadfb08d10ec9468ba40e
Irina Orzan <irinaaa2002@gmail.com>
2017-04-27 12:34:52 +0100

    Fixed Blog CSS

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-   url(r'^$', views.index_blog, name='index')
+   url(r'^$', views.index_blog, name='index'),
+   url(r'^blog', views.index_blog, name='index')
 ]
